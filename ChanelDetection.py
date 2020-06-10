2324554becc8e46eb1bd79319919ea4d598063bc
gchone <gchone@yahoo.ca>
2020-06-10 14:03:12 -0400

    Ajout détection chenal FLT

---
+++ ChanelDetection.py
@@ -0,0 +1,83 @@
+# coding: latin-1
+
+
+
+##################################################################
+# Auteur: François Larouche-Tremblay, Ing, M Sc
+# Description: Algorithme itératif développé par FLT qui permet
+# de détecter la surface de l'eau (cases inondées) à partir d'un raster du chenal
+# ou d'un raster représentant un "HAND zéro". La fonction permet d'exclure
+# les différences d'élévation trop importantes (eltol), de filtrer le résultat final (postpro),
+# d'imposer des zones d'exclusion (offlim) ainsi que d'imposer des brèches (brch).
+##################################################################
+
+# Versions:
+# v1.0 - Sept 2019 - François Larouche-Tremblay - Creation
+# v1.1 - May 2020 - Guénolé Choné - Brought into the FloodTools package. Added global tol
+
+from arcpy import *
+from arcpy.sa import *
+
+
+def execute_ChanelDetection(r_streams, r_dem, niter, offlim, brch, postpro, checkelev, localtol, globaltol, watsurf, messages):
+    env.snapRaster = r_streams
+    env.extent = r_streams
+    niter = max(niter, 1)
+    maskras = None
+
+    if offlim and offlim != "#":
+        maskras = Con(IsNull(offlim), 0)
+
+    newstreams = Con(IsNull(r_streams), r_streams, 0) # replace channel values by 0
+
+    if brch and brch != "#":
+        brchras = CreateScratchName("det", data_type="RasterDataset", workspace="in_memory")
+        PolylineToRaster(brch, "OBJECTID", brchras, "MAXIMUM_LENGTH", cellsize=r_dem)
+        newstreams = Con(IsNull(brchras), newstreams, 0)
+
+    growthcheck = False
+    for ii in range(0, niter, 1):
+        localmax = FocalStatistics((r_dem + newstreams), NbrRectangle(3, 3, "CELL"), "MAXIMUM", "DATA")
+
+        newstreams = Con((r_dem - localmax) <= globaltol, 0)
+        if maskras is not None:  # On ajoute des zéros partout et les NoData du masque effacent les données
+            newstreams = newstreams + maskras
+
+        del localmax
+        if growthcheck:
+            checkras = Con(IsNull(newstreams), 0, 1) - oldgrowth
+
+            if checkras.maximum == 0:
+                niter = ii + 1  # Garde en mémoire le nombre réel d'itérations pour le postprocessing
+                del checkras, oldgrowth
+                break
+            growthcheck = False
+            del checkras, oldgrowth
+
+        if ((ii+1) % 10) == 0:  # Test si le raster des sources a évolué à chaque 10 nouvelles itérations
+            oldgrowth = Con(IsNull(newstreams), 0, 1)
+            growthcheck = True
+
+    if postpro:
+        streamelev = r_dem + newstreams
+        newstreams = newstreams + 1  # Convertit les zéros en 1 pour le compte du nombre de voisins
+        niter = niter//10 + 1  # Hardcoded: environ 10% d'itérations pour le postprocessing
+        for ii in range(0, niter, 1):
+            nbneighbors = FocalStatistics(newstreams, NbrRectangle(3, 3, "CELL"), "SUM", "DATA")
+            if checkelev:
+                nottoohigh = r_dem - FocalStatistics(streamelev, NbrRectangle(5, 5, "CELL"), "MEAN", "DATA")
+                newstreams = Con((nbneighbors >= 6) & (nottoohigh <= localtol), 1, newstreams)
+                del nbneighbors, nottoohigh
+            else:
+                newstreams = Con(nbneighbors >= 6, 1, newstreams)
+                del nbneighbors
+
+            if maskras is not None:  # On ajoute des zéros partout et les NoData du masque effacent les données
+                newstreams = newstreams + maskras
+        del streamelev
+    del maskras
+
+    sources = Con(IsNull(newstreams), r_streams, r_dem)
+    sources.save(watsurf)
+
+    return
---
+++ ChanelDetection_Interface.py
@@ -0,0 +1,130 @@
+# coding: latin-1
+
+##################################################################
+# Auteur: François Larouche-Tremblay, Ing, M Sc
+# Date: 20/09/2019
+# Description: Interface de la fonction "ChanelDetection"
+##################################################################
+
+# Versions:
+# v1.0 - Sept 2019 - François Larouche-Tremblay - Creation
+# v1.1 - May 2020 - Guénolé Choné - Brought into the FloodTools package
+
+from ChanelDetection import *
+import arcpy
+
+class ChanelDetection(object):
+    def __init__(self):
+
+        self.label = "Détection de l'étendue des sources"
+        self.description = "Identifie les cours d'eau de manière itérative"
+        self.canRunInBackground = True
+
+    def getParameterInfo(self):
+        param_streams = arcpy.Parameter(
+            displayName="Élévations du chenal ou du HAND zéro",
+            name="streams",
+            datatype="GPRasterLayer",
+            parameterType="Required",
+            direction="Input")
+        param_dem = arcpy.Parameter(
+            displayName="MNE du bassin versant",
+            name="dem",
+            datatype="GPRasterLayer",
+            parameterType="Required",
+            direction="Input")
+        param_niter = arcpy.Parameter(
+            displayName="Nombre maximal d'itérations de l'algorithme",
+            name="niter",
+            datatype="GPDouble",
+            parameterType="Required",
+            direction="Input")
+        param_globaltol = arcpy.Parameter(
+            displayName="Différence de hauteur maximale tolérée",
+            name="global",
+            datatype="GPDouble",
+            parameterType="Required",
+            direction="Input")
+        param_offlim = arcpy.Parameter(
+            displayName="Raster des zones exclues de l'analyse",
+            name="offlim",
+            datatype="GPRasterLayer",
+            parameterType="Optional",
+            direction="Input")
+        param_brch = arcpy.Parameter(
+            displayName="Lignes des chenaux",
+            name="polybed",
+            datatype="GPFeatureLayer",
+            parameterType="Optional",
+            direction="Input")
+        param_postpro = arcpy.Parameter(
+            displayName="Post-traitement des sources (remplissage des orphelins et surfaces rugeuses)",
+            name="postpro",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        param_checkelev = arcpy.Parameter(
+            displayName="Post-validation des élévations",
+            name="checkelev",
+            datatype="GPBoolean",
+            parameterType="Required",
+            direction="Input")
+        param_eltol = arcpy.Parameter(
+            displayName="Différence de hauteur maximale tolérée entre un orphelin et ses voisins",
+            name="eltol",
+            datatype="GPDouble",
+            parameterType="Required",
+            direction="Input")
+        param_watsurf = arcpy.Parameter(
+            displayName="Raster de la surface de l'eau",
+            name="watsurf",
+            datatype="DERasterDataset",
+            parameterType="Required",
+            direction="Output")
+
+        param_niter.value = 20
+        param_globaltol.value = 0.5
+        param_brch.filter.list = ["Polyline"]
+
+
+        params = [param_streams, param_dem, param_niter, param_globaltol, param_offlim, param_brch, param_postpro, param_checkelev, param_eltol, param_watsurf]
+
+        return params
+
+    def isLicensed(self):
+        return True
+
+    def updateParameters(self, parameters):
+        if parameters[7].valueAsText == 'true':
+            parameters[8].enabled = True
+            parameters[8].value = 0.5
+        else:
+            parameters[8].enabled = False
+
+
+        return
+
+    def updateMessages(self, parameters):
+        return
+
+    def execute(self, parameters, messages):
+
+
+        # Récupération des paramètres
+        streams = parameters[0].valueAsText
+        dem = parameters[1].valueAsText
+        niter = int(parameters[2].valueAsText)
+        globaltol = float(parameters[3].valueAsText)
+        offlim = parameters[4].valueAsText
+
+        brch = parameters[5].valueAsText
+        postpro = bool(parameters[6].valueAsText)
+        checkelev = bool(parameters[7].valueAsText)
+        localtol = float(parameters[8].valueAsText)
+        watsurf = parameters[9].valueAsText
+
+        execute_ChanelDetection(arcpy.Raster(streams), arcpy.Raster(dem), niter, arcpy.Raster(offlim), brch, postpro,  checkelev, localtol, globaltol, watsurf,
+                                messages)
+
+
+        return
