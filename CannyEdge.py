7c87b37094d63b5fd63a51626324c79915c277e6
gchone <gchone@yahoo.ca>
2020-09-18 10:50:29 -0400

    Canny edge detection tool debugged

--- CannyEdge.py
+++ CannyEdge.py
@@ -9,8 +9,8 @@ import os
 def execute_CannyEdge(str_raster, sigma, threshold1, threshold2, result, messages):
 
 
-    randomname = binascii.hexlify(os.urandom(6))
-    filetemp = arcpy.env.scratchWorkspace + "\\" + randomname + ".tif"
+    randomname = binascii.hexlify(os.urandom(6)).decode()
+    filetemp = arcpy.env.scratchWorkspace + "\\" + str(randomname) + ".tif"
 
 
     arcpy.CopyRaster_management(str_raster, filetemp, pixel_type="8_BIT_UNSIGNED")
@@ -19,8 +19,10 @@ def execute_CannyEdge(str_raster, sigma, threshold1, threshold2, result, message
 
     originalraster = arcpy.Raster(str_raster)
 
-    # Blur: to reproduce the OPALS Canny edge behaviour, the kernel size is set to 3 times the standard deviation
-    blur = cv2.GaussianBlur(img, (int(3*sigma), int(3*sigma)), sigma)
+    # Blur: to reproduce the OPALS Canny edge behaviour, the kernel radius is set to 3 times the standard deviation
+    # the kernel size should be however minimum 3, and size%2 ==1 (i.e. is can be 3, 5 ,7 ,9, etc.)
+    kernel_size = max(1, int(round(sigma*3)))*2+1
+    blur = cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma)
     edges = cv2.Canny(blur, threshold1, threshold2)
     resraster = arcpy.NumPyArrayToRaster(edges, arcpy.Point(originalraster.extent.XMin, originalraster.extent.YMin),
                                          originalraster.meanCellWidth, originalraster.meanCellHeight, originalraster.noDataValue)
