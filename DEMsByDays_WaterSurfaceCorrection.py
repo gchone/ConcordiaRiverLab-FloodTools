9f3046377e880a754f992c03e88b24e384d07566
gchone <gchone@yahoo.ca>
2020-12-07 11:46:00 -0500

    Debug

--- DEMsByDays_WaterSurfaceCorrection.py
+++ DEMsByDays_WaterSurfaceCorrection.py
@@ -16,14 +16,14 @@ import sys
 from RasterIO import *
 from ChannelCorrection import *
 
-def execute_WaterSurfaceCorrection(correctedDEMs_dir, channel_poly_dir, polycuts, river_network, output_folder, messages):
+def execute_WaterSurfaceCorrection(correctedDEMs_dir, channel_poly_dir, prefixe, polycuts, river_network, output_folder, messages):
 
 
     arcpy.env.workspace = correctedDEMs_dir
     rasterlist = arcpy.ListRasters()
 
     for raster in rasterlist:
-        channelpoly = os.path.join(channel_poly_dir, raster+".shp")
+        channelpoly = os.path.join(channel_poly_dir, prefixe+raster+".shp")
 
         execute_ChannelCorrection(arcpy.Raster(raster), polycuts, channelpoly, river_network, os.path.join(output_folder, raster), messages)
 
@@ -49,10 +49,11 @@ if __name__ == "__main__":
 
     correctedDEMs_dir = r"path"
     channel_poly_dir = r"path"
+    prefixe = "ch_"
     polycuts = r"path"
     river_network = r"path"
     output_folder = r"path"
 
-    execute_WaterSurfaceCorrection(correctedDEMs_dir, channel_poly_dir, polycuts, river_network, output_folder, messages)
+    execute_WaterSurfaceCorrection(correctedDEMs_dir, channel_poly_dir, prefixe, polycuts, river_network, output_folder, messages)
 
 
