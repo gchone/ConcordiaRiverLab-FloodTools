118c0e923b16eb55619eea16ad91c60711a2ffa3
gchone <gchone@yahoo.ca>
2021-01-20 09:38:16 -0500

    DEM by days: resample and breach

--- DEMsByDays_Resample.py
+++ DEMsByDays_Resample.py
@@ -15,26 +15,41 @@ import os
 import sys
 from RasterIO import *
 
-def execute_Resample(resamle_dir, flow_dir, frompoints_dir, prefixe, river_tools_folder, output_folder, messages):
+def execute_Resample(smoothed_dir, dem3m_dir, flow_dir, frompoints_dir, prefixe, river_tools_folder, output_folder, messages):
 
     sys.path.append(river_tools_folder)
     from Breach import execute_Breach
 
-    arcpy.env.workspace = resamle_dir
+
+
+    arcpy.env.workspace = smoothed_dir
     rasterlist = arcpy.ListRasters()
 
-    flowdir_output = arcpy.CreateScratchName("rfdir", data_type="RasterDataset", workspace=arcpy.env.scratchWorkspace)
+    flowdir_output = arcpy.CreateScratchName("rres1", data_type="RasterDataset", workspace=arcpy.env.scratchWorkspace)
+    temp_resample = arcpy.CreateScratchName("rres2", data_type="RasterDataset", workspace=arcpy.env.scratchWorkspace)
+    temp_tobreach = arcpy.CreateScratchName("rres3", data_type="RasterDataset", workspace=arcpy.env.scratchWorkspace)
 
     for raster in rasterlist:
         print (raster)
 
-        arcpy.Clip_management(flow_dir, raster, flowdir_output, maintain_clipping_extent="MAINTAIN_EXTENT")
+        buffered = arcpy.sa.Float(arcpy.sa.EucAllocation(arcpy.sa.Int(arcpy.Raster(raster) * 1000))) / 1000
+        with arcpy.EnvManager(snapRaster=flow_dir):
+            arcpy.Resample_management(buffered, temp_resample, str(flow_dir.meanCellWidth)+" "+str(flow_dir.meanCellHeight), "BILINEAR")
+        dem3m = os.path.join(dem3m_dir, raster)
+        tobreach = arcpy.sa.ExtractByMask(temp_resample, dem3m)
+        tobreach.save(temp_tobreach)
+
+        newextent = str(tobreach.extent.XMin) + " " + str(tobreach.extent.YMin) + " " + str(tobreach.extent.XMax) + " " + str(tobreach.extent.YMax)
+        arcpy.Clip_management(flow_dir, newextent, flowdir_output, maintain_clipping_extent="MAINTAIN_EXTENT")
 
         str_frompoints = os.path.join(frompoints_dir, prefixe + raster + ".shp")
         result = os.path.join(output_folder, raster)
-        execute_Breach(raster, flowdir_output, str_frompoints, result, messages)
 
-    arcpy.Delete_management(flowdir_output)
+        execute_Breach(arcpy.Raster(temp_tobreach), arcpy.Raster(flowdir_output), str_frompoints, result, messages)
+
+    #arcpy.Delete_management(flowdir_output)
+    #arcpy.Delete_management(temp_resample)
+    #arcpy.Delete_management(temp_tobreach)
 
 class Messages():
     def addErrorMessage(self, text):
@@ -52,13 +67,14 @@ if __name__ == "__main__":
     arcpy.env.scratchWorkspace = r"F:\MSP2\tmp"
     messages = Messages()
 
-    resamle_dir = r"D:\InfoCrue\Etchemin\DEMbydays\PythonProcessing\Resampled10m"
+    smoothed_dir = r"D:\InfoCrue\Etchemin\DEMbydays\PythonProcessing\Smoothed"
+    dem3m_dir = r"D:\InfoCrue\Etchemin\DEMbydays\PythonProcessing\DEM3m"
     flow_dir = arcpy.Raster(r"D:\InfoCrue\Etchemin\DEMbydays\lidar10m_fd")
     frompoints_dir = r"D:\InfoCrue\Etchemin\DEMbydays\PythonProcessing\FromPoints"
     prefixe = "fp_"
     river_tools_folder = r"F:\PyCharm\GISTools\RiversTools"
     output_folder = r"D:\InfoCrue\Etchemin\DEMbydays\PythonProcessing\ResultWS"
 
-    execute_Resample(resamle_dir, flow_dir, frompoints_dir, prefixe, river_tools_folder, output_folder, messages)
+    execute_Resample(smoothed_dir, dem3m_dir, flow_dir, frompoints_dir, prefixe, river_tools_folder, output_folder, messages)
 
 
