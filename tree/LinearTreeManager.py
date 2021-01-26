4aca5b47352e2cc4744f5cc57548dc2f215ae46c
gchone <gchone@yahoo.ca>
2021-01-26 09:39:47 -0500

    Tree re-organization

--- tree/LinearTreeManager.py
+++ tree/LinearTreeManager.py
@@ -14,7 +14,7 @@ import numpy.lib.recfunctions as rfn
 
 
 
-class LinearTreeManager(TreeManager.TreeManager):
+class LinearTreeManager(object):
 
     ### Instance attributes ###
     # net: a line shapefile (normally with M)
@@ -22,8 +22,131 @@ class LinearTreeManager(TreeManager.TreeManager):
 
 
     def __init__(self):
-        TreeManager.TreeManager.__init__(self)
+        self.treeroot = None
 
+
+    def treesegments(self):
+        #   retour de la méthode : Générateur de TreeSegment
+        for n in self.__recursivetreesegments(self.treeroot):
+            yield n
+
+    def treesegments_uptodown(self):
+        #   retour de la méthode : Générateur de TreeSegment
+        for leaf in self.leaves():
+            for n in self.__recursivetreesegments_uptodown(leaf):
+                yield n
+
+    def __recursivetreesegments(self, treesegment):
+        #   treesegment : TreeSegment - Segment à retourner, ainsi que ces enfants (segments amont)
+        #   retour de la méthode : Générateur de TreeSegment
+        yield treesegment
+        for child in treesegment.get_childrens():
+            for n in self.__recursivetreesegments(child):
+                yield n
+
+    def __recursivetreesegments_uptodown(self, treesegment):
+        #   treesegment : TreeSegment - Segment à retourner, ainsi que son parent (segment aval)
+        #   retour de la méthode : Générateur de TreeSegment
+        yield treesegment
+        if not treesegment.is_root():
+            for n in self.__recursivetreesegments_uptodown(treesegment.get_parent()):
+                yield n
+
+    def get_treesegment(self, id):
+        #   id : int - Identifiant d'un segment
+        #   retour de la méthode : TreeSegment
+        for segment in self.treesegments():
+            if segment.id == id:
+                return segment
+
+    def leaves(self):
+        #   retour de la méthode : liste de Segments (les extrémités amont)
+        leaves = []
+        for segment in self.treesegments():
+            if segment.is_leaf():
+                leaves.append(segment)
+        return leaves
+
+    def __str__(self):
+        return str(self.treeroot)
+
+
+
+    def getsegmentbyprofilpt(self, row, col):
+        for segment in self.treesegments():
+            for ptprofil in segment.get_profile():
+                if ptprofil.row == row and ptprofil.col == col:
+                    return segment, ptprofil
+        return None, None
+
+    def browsepts(self):
+        #   retour de la méthode : Générateur de ProfilePoint
+        for l, m, n in self.__recursivetreepts(self.treeroot):
+            yield l, m, n
+
+
+    def uptodown_browsepts(self):
+        #   retour de la méthode : Générateur de ProfilePoint
+        for leaf in self.leaves():
+            for l, m, n in self.__recursiveuptodowntreepts(leaf, None):
+                yield l, m, n
+
+    def __recursiveuptodowntreepts(self, treesegment, preccs):
+
+        yield treesegment, preccs, treesegment.get_profile()[-1]
+
+        for i in range(len(treesegment.get_profile()) - 2, -1, -1):
+            yield treesegment, treesegment.get_profile()[i + 1], treesegment.get_profile()[i]
+
+        if not treesegment.is_root():
+            for l, m, n in self.__recursiveuptodowntreepts(treesegment.get_parent(), treesegment.get_profile()[0]):
+                yield l, m, n
+
+    def points_up_with_data(self, segment, cs, cs_attribute, cs_attribute_nodata_value):
+        csposition = segment.get_ptprofile_pos(cs)
+        for csup in self.__recursive_points_up_with_data(segment, csposition, cs_attribute, cs_attribute_nodata_value):
+            yield csup
+
+    def __recursive_points_up_with_data(self, segment, csposition, cs_attribute, cs_attribute_nodata_value):
+        foundup = False
+        pos = 0
+        for csup in segment.get_profile():
+            if pos > csposition and not foundup:
+                if hasattr(csup, cs_attribute) and getattr(csup, cs_attribute) != cs_attribute_nodata_value:
+                    foundup = True
+                    yield csup
+            pos += 1
+        if not foundup:
+            for child in segment.get_childrens():
+                for csup in self.__recursive_points_up_with_data(child, -1, cs_attribute, cs_attribute_nodata_value):
+                    yield csup
+
+    def __recursivetreepts(self, treesegment):
+        if treesegment.is_root():
+            yield treesegment, None, treesegment.get_profile()[0]
+        else:
+            yield treesegment, treesegment.get_parent().get_profile()[-1], treesegment.get_profile()[0]
+        for i in range(1, len(treesegment.get_profile())):
+            yield treesegment, treesegment.get_profile()[i - 1], treesegment.get_profile()[i]
+        for child in treesegment.get_childrens():
+            for l, m, n in self.__recursivetreepts(child):
+                yield l, m, n
+
+    def treesegments_prioritize_by_attribute(self, cs_attribute):
+        #   retour de la méthode : Générateur de TreeSegment
+        for n in self.__recursivetreesegments_prioritize_by_attribute(self.treeroot, cs_attribute):
+            yield n
+
+    def __recursivetreesegments_prioritize_by_attribute(self, treesegment, cs_attribute):
+        #   treesegment : TreeSegment - Segment à retourner, ainsi que ces enfants (segments amont)
+        #   retour de la méthode : Générateur de TreeSegment
+        yield treesegment
+        # sorting upstream reaches by an attribute (from higher to lower)
+        upstream_segments = list(treesegment.get_childrens())
+        upstream_segments.sort(key=lambda segment: segment.get_profile()[0].getattr(cs_attribute), reverse=True)
+        for child in upstream_segments:
+            for n in self.__recursivetreesegments_prioritize_by_attribute(child, cs_attribute):
+                yield n
 
     def interpolate(self, sourcepoints, value_field, source_meas_field, source_routeID_field, targetpoints, target_meas_field, target_routeID_field):
         """
@@ -124,7 +247,7 @@ def build_trees(rivernet, routeID_field, length_field):
 def nonoriented_build_trees(rivernet, routeID_field, downstream_reach_field):
     __generic_build_trees(rivernet, routeID_field, oriented=False, downstream_reach_field=downstream_reach_field, length_field=None)
 
-def __generic_build_trees(rivernet, routeID_field, downstream_reach_field, length_field):
+def __generic_build_trees(rivernet, routeID_field, oriented, downstream_reach_field, length_field):
     """
 
     :param rivernet:
@@ -202,4 +325,132 @@ def __generic_build_trees(rivernet, routeID_field, downstream_reach_field, lengt
     return trees
 
 
+def build_trees_from_FlowDir(flowdir, frompoint):
 
+    trees = []
+    segmentid = 0
+
+    treated_pts = {}
+
+    # Traitement effectué pour chaque point de départ
+    frompointcursor = arcpy.da.SearchCursor(frompoint, ["SHAPE@", "OID@"])
+    for frompoint in frompointcursor:
+
+        # On prend l'objet géométrique (le point) associé à la ligne dans la table
+        frompointshape = frompoint[0].firstPoint
+
+        # Conversion des coordonnées
+        currentcol = flowdir.XtoCol(frompointshape.X)
+        currentrow = flowdir.YtoRow(frompointshape.Y)
+
+        # Tests de sécurité pour s'assurer que le point de départ est à l'intérieurs des rasters
+        intheraster = True
+        if currentcol < 0 or currentcol >= flowdir.raster.width or currentrow < 0 or currentrow >= flowdir.raster.height:
+            intheraster = False
+        elif (flowdir.getValue(currentrow, currentcol) != 1 and flowdir.getValue(currentrow, currentcol) != 2 and
+                      flowdir.getValue(currentrow, currentcol) != 4 and flowdir.getValue(currentrow,
+                                                                                         currentcol) != 8 and
+                      flowdir.getValue(currentrow, currentcol) != 16 and flowdir.getValue(currentrow,
+                                                                                          currentcol) != 32 and flowdir.getValue(
+            currentrow, currentcol) != 64 and flowdir.getValue(currentrow, currentcol) != 128):
+            intheraster = False
+
+        segmentid += 1
+        newtreeseg = OurTreeSegment(segmentid)
+
+
+        # Traitement effectué sur chaque cellule le long de l'écoulement
+        while (intheraster):
+
+
+            dictdata = {}
+            if dtype=="SINGLE":
+                for paramname, param in kwargs.items():
+                    dictdata[paramname] = param.getValue(currentrow, currentcol)
+                ptprofile = ProfilePoint.ProfilePoint(currentrow, currentcol, 0, dictdata)
+            else:
+                # param must be a dictionary of RasterIO object (the data are a collection of rasters in a folder)
+                for paramname, param in kwargs.items():
+                    for raster_name, raster in param.items():
+                        if not dictdata.has_key(raster_name):
+                            dictdata[raster_name] = {}
+                        dictdata[raster_name][paramname] = raster.getValue(currentrow, currentcol)
+                ptprofile = ProfilePoint.ProfilePointMulti(currentrow, currentcol, 0, dictdata)
+            ptprofile.X = flowdir.ColtoX(currentcol)
+            ptprofile.Y = flowdir.RowtoY(currentrow)
+            newtreeseg.add_ptprofile(ptprofile)
+            treated_pts[(currentrow, currentcol)] = segmentid
+
+            # On cherche le prochain point à partir du flow direction
+            direction = flowdir.getValue(currentrow, currentcol)
+            if (direction == 1):
+                currentcol = currentcol + 1
+                currentdistance = flowdir.raster.meanCellWidth
+            if (direction == 2):
+                currentcol = currentcol + 1
+                currentrow = currentrow + 1
+                currentdistance = math.sqrt(
+                    flowdir.raster.meanCellWidth * flowdir.raster.meanCellWidth + flowdir.raster.meanCellHeight * flowdir.raster.meanCellHeight)
+            if (direction == 4):
+                currentrow = currentrow + 1
+                currentdistance = flowdir.raster.meanCellHeight
+            if (direction == 8):
+                currentcol = currentcol - 1
+                currentrow = currentrow + 1
+                currentdistance = math.sqrt(
+                    flowdir.raster.meanCellWidth * flowdir.raster.meanCellWidth + flowdir.raster.meanCellHeight * flowdir.raster.meanCellHeight)
+            if (direction == 16):
+                currentcol = currentcol - 1
+                currentdistance = flowdir.raster.meanCellWidth
+            if (direction == 32):
+                currentcol = currentcol - 1
+                currentrow = currentrow - 1
+                currentdistance = math.sqrt(
+                    flowdir.raster.meanCellWidth * flowdir.raster.meanCellWidth + flowdir.raster.meanCellHeight * flowdir.raster.meanCellHeight)
+            if (direction == 64):
+                currentrow = currentrow - 1
+                currentdistance = flowdir.raster.meanCellHeight
+            if (direction == 128):
+                currentcol = currentcol + 1
+                currentrow = currentrow - 1
+                currentdistance = math.sqrt(
+                    flowdir.raster.meanCellWidth * flowdir.raster.meanCellWidth + flowdir.raster.meanCellHeight * flowdir.raster.meanCellHeight)
+
+            ptprofile.dist = currentdistance
+
+            # Tests de sécurité pour s'assurer que l'on ne sorte pas des rasters
+            if currentcol < 0 or currentcol >= flowdir.raster.width or currentrow < 0 or currentrow >= flowdir.raster.height:
+                intheraster = False
+            elif (flowdir.getValue(currentrow, currentcol) != 1 and flowdir.getValue(currentrow,
+                                                                                     currentcol) != 2 and
+                          flowdir.getValue(currentrow, currentcol) != 4 and flowdir.getValue(currentrow,
+                                                                                             currentcol) != 8 and
+                          flowdir.getValue(currentrow, currentcol) != 16 and flowdir.getValue(currentrow,
+                                                                                              currentcol) != 32 and flowdir.getValue(
+                currentrow, currentcol) != 64 and flowdir.getValue(currentrow, currentcol) != 128):
+                intheraster = False
+
+            if intheraster:
+                if (currentrow, currentcol) in treated_pts:
+                    # Atteinte d'un confluent
+                    nextcellsegment = treated_pts[(currentrow, currentcol)]
+                    intheraster = False
+                    # tree.get_treesegment(nextcellsegment).add_child(fp_tree)
+                    # confluence = True
+                    # confluenceid = nextcellsegment
+                    segmentid += 1
+                    # on cherche l'arbre et le segment que l'on vient de rejoindre
+                    for tree in trees:
+                        oldsegment, ptprofile = tree.getsegmentbyprofilpt(currentrow, currentcol)
+                        if oldsegment is not None:
+                            break
+                    oldsegment.fork(newtreeseg, segmentid, ptprofile)
+
+
+
+            else:
+                tree = OurTreeManager()
+                tree.treeroot = newtreeseg
+                trees.append(tree)
+
+    return trees
