74505e4ad7fc57fb7bb44972779b0df4996ea2a8
gchone <gchone@yahoo.ca>
2021-03-18 11:21:24 -0400

    Structure definition

--- tree/DataPoint.py
+++ tree/DataPoint.py
@@ -3,7 +3,9 @@
 ### Historique des versions ###
 # v2.0 - Fev 2020 - Création
 
-class DataPoint(object):
+from tree.NumpyArrayFedObject import *
+
+class DataPoint(NumpyArrayFedObject):
 
     def __init__(self, id, downstream_point, reach, distance, offset):
         self.downstream_point = downstream_point
