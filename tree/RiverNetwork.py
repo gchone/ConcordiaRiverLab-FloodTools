a59c9cbb973f8fcc65407f14788912947a1b234e
gchone <gchone@yahoo.ca>
2021-03-19 11:44:45 -0400

    Structure definition

--- tree/RiverNetwork.py
+++ tree/RiverNetwork.py
@@ -11,8 +11,8 @@ import numpy.lib.recfunctions as rfn
 
 class RiverNetwork(object):
 
-    reaches_linkfieldup = "UpstreamRID"
-    reaches_linkfielddown = "DownstreamRID"
+    _reaches_linkfieldup = "UpstreamRID"
+    _reaches_linkfielddown = "DownstreamRID"
 
     def __init__(self, reaches_shapefile, reaches_linktable, dict_attr_fields):
 
@@ -20,44 +20,42 @@ class RiverNetwork(object):
         self._dict_points_collection = {}
 
         # on initialise les matrices Numpy
+        # matrice de base
         self._numpyarray = arcpy.da.FeatureClassToNumPyArray(reaches_shapefile, dict_attr_fields.values(), null_value=-9999)
-
-        self._numpyarraylinks = arcpy.da.TableToNumPyArray(reaches_linktable, [self.reaches_linkfielddown, self.reaches_linkfieldup])
-
-
-        # on ajoute un champ contenant une nouvelle instance de Reach
-        object_column = np.empty(self._numpyarray.shape[0], dtype=[("object", 'O')])
-        #object_column = np.empty(self.__numpyarray.shape[0], dtype=object)
-        #object_column = np.full(self._numpyarray.shape[0], 4, dtype=[("sdads",'<i4')])
-        #object_column = np.empty(self._numpyarray.shape[0], dtype=[("sdads", '<i4')])
-
-        #object_column = np.array([Reach(self, row[self._dict_attr_fields[Reach.idkey]]) for row in self._numpyarray])
-        print self._numpyarray
-        print self._numpyarray.dtype
-        print object_column
-        print object_column.dtype
+        # matrice des liaisons amont-aval
+        self._numpyarraylinks = arcpy.da.TableToNumPyArray(reaches_linktable, [self._reaches_linkfielddown, self._reaches_linkfieldup])
+        # matrice contenant les instances de Reach
+        self._reaches = np.empty(self._numpyarray.shape[0], dtype=[('id', 'i4'),('object', 'O')])
+        for i in range(self._numpyarray.shape[0]):
+            self._reaches[i]['id'] = self._numpyarray[i][self._dict_attr_fields['id']]
+            self._reaches[i]['object'] = Reach(self, self._numpyarray[i][self._dict_attr_fields['id']])
+        # Tried different things to add a field with the objects to the _numpyarray but did not succeed
         #self._numpyarray = rfn.merge_arrays([self._numpyarray, object_column], flatten=True)
-        rfn.append_fields(self._numpyarray, "object", object_column)
+        #rfn.append_fields(self._numpyarray, 'object', list)
 
-        for row in self._numpyarray:
-            row['object'] = Reach(self, row[self._dict_attr_fields[Reach.idkey]])
+    def get_downstream_ends(self):
+        # Générateur. Retourne la liste des tronçons extrémités aval
+        # Ce sont ceux qui ne sont pas présents dans "UpstreamRID" de _numpyarraylinks
+        for id in np.setdiff1d(self._reaches['id'], self._numpyarraylinks[self._reaches_linkfieldup]):
+            yield self._reaches[self._reaches['id'] == id]['object'][0]
 
-
-    def __get_downstream_reach(self, id):
-        ### trouve et retourne le segment aval d'un segment dans la matrice Numpy  ###
-        return None
-
-    def __get_upstream_reaches(self, id):
-        ### trouve et retourne la liste de segments amont d'un segment dans la matrice Numpy  ###
-        return []
+    # def __get_downstream_reach(self, id):
+    #     ### trouve et retourne le segment aval d'un segment dans la matrice Numpy  ###
+    #     return None
+    #
+    # def __get_upstream_reaches(self, id):
+    #     ### trouve et retourne la liste de segments amont d'un segment dans la matrice Numpy  ###
+    #     return []
 
     def browse_reaches(self, orientation="DOWN_TO_UP", prioritize_points_collection = "MAIN", prioritize_points_attribute  = None, reverse = False):
         ### Générateur. Trouve et retourne la liste de segments dans la matrice Numpy  ###
         ### La matrice numpy est interrogée pour fournir les tronçons dans l'ordre souhaitée. Optionnellement, si
         ###     prioritize_points_attribute != None, l'ordre des tronçons retournés aux confluences est aussi déterminé.
-        np_request_results = None # requete numpy
-        for np_reachline in np_request_results:
-            yield None # on retourne l'instance de Reach dans la matrice Numpy
+        for downstream_end in self.get_downstream_ends():
+            listup = self._numpyarraylinks[self._reaches_linkfielddown] == downstream_end.id
+            np_request_results = None # requete numpy
+            for np_reachline in np_request_results:
+                yield None # on retourne l'instance de Reach dans la matrice Numpy
 
     def __browse_points_in_reach(self, reachid, points_collection, orientation = "DOWN_TO_UP"):
         ### Générateur. Trouve et retourne la liste de points pour un segment dans la matrice Numpy  ###
@@ -82,11 +80,11 @@ class RiverNetwork(object):
 
 class _NumpyArrayFedObject(object):
 
-    idkey = "tableid"
 
-    def __init__(self, numpyarray_holder):
+    def __init__(self, numpyarray_holder, id):
         # Appel à __dict__ fait pour éviter la récursion infinie liée à __getattr__/__setattr__
         self.__dict__["_numpyarray_holder"] = numpyarray_holder
+        self.__dict__["id"]  = id
 
 
     def __getattr__(self, name):
@@ -104,7 +102,7 @@ class _NumpyArrayFedObject(object):
 
 
     def __setattr__(self, name, value):
-        if name in self._numpyarray_holder._dict_attr_fields.keys():
+        if name != "id" and name in self._numpyarray_holder._dict_attr_fields.keys():
             # change la valeur dans la matrice Numpy pour
             array = self._numpyarray_holder._numpyarray
             # champ pour l'id:
@@ -121,9 +119,9 @@ class Reach(_NumpyArrayFedObject):
 
     def __init__(self, rivernetwork, id):
         #   id : int - identifiant du segment
-        _NumpyArrayFedObject.__init__(self, rivernetwork)
+        _NumpyArrayFedObject.__init__(self, rivernetwork, id)
         self.rivernetwork = rivernetwork
-        self.id = id
+
 
 
     def get_downstream_reach(self):
@@ -144,7 +142,7 @@ class Reach(_NumpyArrayFedObject):
 
     def __str__(self):
         #return self.__recursiveprint("")
-        return "0"
+        return str(self.id)
 
     def __recursiveprint(self, prefix):
         string = prefix + str(self.id) + "\n"
@@ -166,8 +164,7 @@ class Points_collection(object):
 
 class DataPoint(_NumpyArrayFedObject):
     def __init__(self, id, downstream_point, reach, pointscollection):
+        _NumpyArrayFedObject.__init__(self, pointscollection, id)
         self.downstream_point = downstream_point
-        self.id = id
         self.reach = reach
         self.pointscollection = pointscollection
-        _NumpyArrayFedObject.__init__(self, pointscollection)
