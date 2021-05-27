889d04f90f4ff5bc8b56e61d34a725829523e84c
gchone <g.chone@yahoo.ca>
2021-05-27 09:09:16 -0400

    Removing unnecessary error management

--- SolverDirect.py
+++ SolverDirect.py
@@ -4,8 +4,8 @@
 
 g = 9.81
 #Froude_limite = 0.94
-import warnings
-warnings.simplefilter("error", RuntimeWarning)
+#import warnings
+#warnings.simplefilter("error", RuntimeError)
 
 from scipy.optimize import fsolve
 
@@ -43,24 +43,25 @@ def cs_solver(cs_up, cs_down):
         R = (cs_tosolve.width * y) / (cs_tosolve.width + 2 * y)
         v = cs_tosolve.Q / (cs_tosolve.width * y)
         s = (cs_tosolve.n ** 2 * v ** 2) / (R ** (4. / 3.))
-        z = cs_tosolve.wslidar - y
         h = cs_tosolve.wslidar
         # with kinetic energy?
         h = h + v ** 2 / (2 * g)
         # slope calculation:
         #friction_h = (cs_up.dist - cs_down.dist) * s
         friction_h = (cs_up.dist - cs_down.dist) * (s+cs_ref.s)/2.
-        energy = h + friction_h - cs_up.h
+        energy = friction_h  + h - cs_up.h
         return energy
 
     # premier estimé : y = y_crit
     cs_tosolve.ycrit = (cs_tosolve.Q / (cs_tosolve.width * g ** 0.5)) ** (2. / 3.)
+
     cs_tosolve.y = fsolve(equations, cs_tosolve.ycrit)[0]
+
 
     cs_tosolve.R = (cs_tosolve.width * cs_tosolve.y) / (cs_tosolve.width + 2 * cs_tosolve.y)
     cs_tosolve.v = cs_tosolve.Q / (cs_tosolve.width * cs_tosolve.y)
     cs_tosolve.z = cs_tosolve.wslidar - cs_tosolve.y
-    cs_tosolve.h = cs_tosolve.wslidar
+    cs_tosolve.h = cs_tosolve.wslidar + (cs_up.dist - cs_down.dist)*cs_tosolve.s_min
     # with kinetic energy?
     cs_tosolve.h = cs_tosolve.h + cs_tosolve.v ** 2 / (2 * g)
     cs_tosolve.s = (cs_tosolve.n ** 2 * cs_tosolve.v ** 2) / (cs_tosolve.R ** (4. / 3.))
