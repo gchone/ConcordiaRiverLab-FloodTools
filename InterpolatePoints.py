fcaa0cd1371ed307aa7b611590f4aae1ce2fc7d3
gchone <g.chone@yahoo.ca>
2021-07-07 14:12:22 -0400

    Added options in the Interpolation

--- InterpolatePoints.py
+++ InterpolatePoints.py
@@ -7,7 +7,11 @@ import numpy as np
 
 from tree.RiverNetwork import *
 
-def execute_InterpolatePoints(points_table, id_field_pts, RID_field_pts, Distance_field_pts, data_fields, targetpoints, id_field_target, RID_field_target, Distance_field_target, network_shp, links_table, network_RID_field, order_field, ouput_table):
+def execute_InterpolatePoints(points_table, id_field_pts, RID_field_pts, Distance_field_pts, data_fields, targetpoints, id_field_target, RID_field_target, Distance_field_target, network_shp, links_table, network_RID_field, order_field, ouput_table, extrapolation_value=None):
+    # extrapolation_value = None : first upstream (or last downstream value) in the data copied for target points more downstream (or upstream)
+    # extrapolation_value = "CONFLUENCE" : same as extrapolation_value = None, with in addition no interpolation at confluences (first upstream value used) (used for width)
+    # extrapolation_value numeric : value used downstream (or upstream) of data values
+
     network = RiverNetwork()
     network.dict_attr_fields['id'] = network_RID_field
     network.dict_attr_fields['order'] = order_field
@@ -52,42 +56,72 @@ def execute_InterpolatePoints(points_table, id_field_pts, RID_field_pts, Distanc
                 datacollection._numpyarray[datacollection.dict_attr_fields['reach_id']] == reach.id],
             order=datacollection.dict_attr_fields['dist'])
 
-        if not reach.is_downstream_end():
-            # add the most upstream point in the downstream reach in the data
-            down_reach = reach.get_downstream_reach()
-            downdata = np.sort(
-                datacollection._numpyarray[
-                    datacollection._numpyarray[datacollection.dict_attr_fields['reach_id']] == down_reach.id],
-                order=datacollection.dict_attr_fields['dist'])[-1]
-            # update the distance
-            downdata[datacollection.dict_attr_fields['dist']] = downdata[datacollection.dict_attr_fields['dist']] - down_reach.length
-            sorteddata = np.concatenate(([downdata], sorteddata))
+        # browsing down until we find a reach with at least one point
+        down_point = None
+        down_reach = reach
+        downend = reach.is_downstream_end()
+        same_river_down_reach_order = reach.order # this variable is used to identify if we stay on the same river (as opposed as we meet a larger one)
+        while down_point is None and not downend:
+            down_reach = down_reach.get_downstream_reach()
+            same_river_down_reach_order -= 1
+            if extrapolation_value != "CONFLUENCE" or down_reach.order == same_river_down_reach_order:
+                downpoints =np.sort(
+                    datacollection._numpyarray[
+                        datacollection._numpyarray[datacollection.dict_attr_fields['reach_id']] == down_reach.id],
+                    order=datacollection.dict_attr_fields['dist'])
+                if len(downpoints) > 0:
+                    down_point = downpoints[-1]
+                    # update the distance
+                    down_point[datacollection.dict_attr_fields['dist']] = down_point[datacollection.dict_attr_fields[
+                        'dist']] - down_reach.length
+                    # add the most upstream point in the downstream reach in the data
+                    sorteddata = np.concatenate(([down_point], sorteddata))
+                downend = down_reach.is_downstream_end()
+            else:
+                downend = True
 
-        if not reach.is_upstream_end():
-            # add the most downstream point in the upstream reach in the data
-            min_order = 9999
-            selected_up_reach = None
-            for up_reach in reach.get_uptream_reaches():
+        # browsing up until we find a reach with at least one point
+        up_point = None
+        up_reach = reach
+        upend = reach.is_upstream_end()
+
+        while up_point is None and not upend:
+            min_order = None
+            for tmp_up_reach in up_reach.get_uptream_reaches():
                 # the upstream reach to use is the one with the smallest order
-                if up_reach.order < min_order:
-                    min_order = up_reach.order
-                    selected_up_reach = up_reach
+                if min_order is None or tmp_up_reach.order < min_order:
+                    min_order = tmp_up_reach.order
+                    up_reach = tmp_up_reach
 
-            updata = np.sort(
+            uppoints = np.sort(
                 datacollection._numpyarray[
-                    datacollection._numpyarray[datacollection.dict_attr_fields['reach_id']] == selected_up_reach.id],
-                order=datacollection.dict_attr_fields['dist'])[0]
-            # update the distance
-            updata[datacollection.dict_attr_fields['dist']] = updata[datacollection.dict_attr_fields['dist']] + reach.length
-            sorteddata = np.concatenate((sorteddata, [updata]))
+                    datacollection._numpyarray[datacollection.dict_attr_fields['reach_id']] == up_reach.id],
+                order=datacollection.dict_attr_fields['dist'])
+
+            if len(uppoints) > 0:
+                up_point = uppoints[0]
+                # update the distance
+                up_point[datacollection.dict_attr_fields['dist']] = up_point[datacollection.dict_attr_fields['dist']] + reach.length
+                # add the most downstream point in the upstream reach in the data
+                sorteddata = np.concatenate(([up_point], sorteddata))
+            upend = up_reach.is_upstream_end()
 
         newres = np.empty(targetdata.shape[0], dtype=newdtype)
 
+        if extrapolation_value is None or extrapolation_value == "CONFLUENCE":
+            interp_left_right_param = None
+        else:
+            interp_left_right_param = extrapolation_value
+
         for field, value in dict_attr_output_fields.items():
             newres[value] = targetdata[targetcollection.dict_attr_fields[field]]
+
         for field in data_fields:
-            interp= np.interp(targetdata[targetcollection.dict_attr_fields['dist']], sorteddata[datacollection.dict_attr_fields['dist']], sorteddata[field])
-            newres[field] = interp
+            if len(sorteddata) > 0:
+                interp = np.interp(targetdata[targetcollection.dict_attr_fields['dist']], sorteddata[datacollection.dict_attr_fields['dist']], sorteddata[field], left=interp_left_right_param, right=interp_left_right_param)
+                newres[field] = interp
+            else:
+                newres[field] = float(extrapolation_value)
 
         # Create a new array or update it
         if newarray is None:
