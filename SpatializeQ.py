3a87d4fb0a12e1d1c6a65f5d97bffffc59cc6e0a
gchone <g.chone@yahoo.ca>
2023-02-28 14:19:58 -0500

    SpatializeQ: debug error linked with NoData / 0 values

--- SpatializeQ.py
+++ SpatializeQ.py
@@ -14,7 +14,7 @@
 
 import os
 import arcpy
-
+import random
 from RasterIO import *
 from tree.OurTreeSegment import *
 from tree.OurTreeManager import *
@@ -29,21 +29,18 @@ def execute_SpatializeQ(r_flowdir, str_frompoint, r_flowacc, r_qpts, str_res, in
     flowacc = RasterIO(r_flowacc)
     qpts = RasterIO(r_qpts)
 
-    Res = RasterIO(r_flowdir, str_res, float, -255)
+    Res = RasterIO(r_flowdir, str_res, float, flowacc.nodata)
     trees = build_trees(flowdir,str_frompoint,Q=qpts,flowacc=flowacc)
 
 
     for tree in trees:
         # transmitting from upstream point
         for segment, prev_cs, cs in tree.uptodown_browsepts():
-
             cs.A = float(cs.flowacc)
-
-
-            if cs.Q != qpts.nodata:
+            if cs.Q != qpts.nodata and cs.Q != 0: ## cs.Q != 0 ajouté à cause d'un problème de NoData. Pas propre.
                 cs.up_ptsA = cs.A
                 cs.up_ptsQ = cs.Q
-            elif prev_cs != None:
+            elif prev_cs is not None:
                 cs.up_ptsA = prev_cs.up_ptsA
                 cs.up_ptsQ = prev_cs.up_ptsQ
             else:
@@ -54,22 +51,25 @@ def execute_SpatializeQ(r_flowdir, str_frompoint, r_flowacc, r_qpts, str_res, in
         for segment, prev_cs, cs in tree.browsepts():
 
 
-            if cs.Q != qpts.nodata:
+
+            if cs.Q != qpts.nodata and cs.Q != 0: ## cs.Q != 0 ajouté à cause d'un problème de NoData. Pas propre.
                 cs.down_ptsA = cs.A
                 cs.down_ptsQ = cs.Q
 
             else:
-                if prev_cs != None:
+                if prev_cs is not None:
                     cs.down_ptsA = prev_cs.down_ptsA
                     cs.down_ptsQ = prev_cs.down_ptsQ
                 else:
                     cs.down_ptsA = 0
                     cs.down_ptsQ = 0
 
+
                 if interpolation:
                     cs.alpha = (cs.up_ptsQ - cs.down_ptsQ) / (cs.up_ptsA - cs.down_ptsA)
                     cs.beta = cs.down_ptsQ - cs.alpha * cs.down_ptsA
                     cs.Q = cs.alpha * cs.A + cs.beta
+                    print(cs.Q)
                 else:
                     if cs.down_ptsA == 0:
                         # cas particulier : pas d'interpolation (débit de crue), et extrapolation down
